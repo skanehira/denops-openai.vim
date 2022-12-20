284d2a37f8e3088072555f9771ddf7d64f2abb52
skanehira <skanehira@users.noreply.github.com>
2022-12-20 00:18:53 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-openai.vim/denops-openai.vim/denops/openai/deps.ts
    [1/9] Looking for releases: https://deno.land/x/denops_std@v3.12.0/mod.ts
    [1/9] Using latest: https://deno.land/x/denops_std@v3.12.0/mod.ts
    [2/9] Looking for releases: https://deno.land/x/denops_std@v3.12.0/variable/mod.ts
    [2/9] Using latest: https://deno.land/x/denops_std@v3.12.0/variable/mod.ts
    [3/9] Looking for releases: https://deno.land/x/unknownutil@v2.1.0/mod.ts
    [3/9] Using latest: https://deno.land/x/unknownutil@v2.1.0/mod.ts
    [4/9] Looking for releases: https://deno.land/std@0.168.0/testing/asserts.ts
    [4/9] Attempting update: https://deno.land/std@0.168.0/testing/asserts.ts -> 0.170.0
    [4/9] Update successful: https://deno.land/std@0.168.0/testing/asserts.ts -> 0.170.0
    [5/9] Looking for releases: https://deno.land/x/zod@v3.20.2/mod.ts
    [5/9] Using latest: https://deno.land/x/zod@v3.20.2/mod.ts
    [6/9] Looking for releases: https://deno.land/x/denops_std@v3.12.0/helper/mod.ts
    [6/9] Using latest: https://deno.land/x/denops_std@v3.12.0/helper/mod.ts
    [7/9] Looking for releases: https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts
    [7/9] Using latest: https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts
    [8/9] Looking for releases: https://deno.land/x/denops_std@v3.12.0/variable/mod.ts
    [8/9] Using latest: https://deno.land/x/denops_std@v3.12.0/variable/mod.ts
    [9/9] Looking for releases: https://deno.land/x/denops_std@v3.12.0/mapping/mod.ts
    [9/9] Using latest: https://deno.land/x/denops_std@v3.12.0/mapping/mod.ts
    
    Already latest version:
    https://deno.land/x/denops_std@v3.12.0/mod.ts == v3.12.0
    https://deno.land/x/denops_std@v3.12.0/variable/mod.ts == v3.12.0
    https://deno.land/x/unknownutil@v2.1.0/mod.ts == v2.1.0
    https://deno.land/x/zod@v3.20.2/mod.ts == v3.20.2
    https://deno.land/x/denops_std@v3.12.0/helper/mod.ts == v3.12.0
    https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts == v3.12.0
    https://deno.land/x/denops_std@v3.12.0/variable/mod.ts == v3.12.0
    https://deno.land/x/denops_std@v3.12.0/mapping/mod.ts == v3.12.0
    
    Successfully updated:
    https://deno.land/std@0.168.0/testing/asserts.ts 0.168.0 -> 0.170.0

--- denops/openai/deps.ts
+++ denops/openai/deps.ts
@@ -1,7 +1,7 @@
 export * from "https://deno.land/x/denops_std@v3.12.0/mod.ts";
 export * from "https://deno.land/x/denops_std@v3.12.0/variable/mod.ts";
 export * from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
-export * from "https://deno.land/std@0.168.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.170.0/testing/asserts.ts";
 export * from "https://deno.land/x/zod@v3.20.2/mod.ts";
 export * as helper from "https://deno.land/x/denops_std@v3.12.0/helper/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v3.12.0/autocmd/mod.ts";
