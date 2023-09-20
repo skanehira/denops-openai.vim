753ece8961388c78db99f5cee4e7cbbfb67fe096
skanehira <skanehira@users.noreply.github.com>
2023-09-20 00:17:03 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-openai.vim/denops-openai.vim/denops/openai/deps.ts
    [1/11] Looking for releases: https://deno.land/x/denops_std@v5.0.1/mod.ts
    [1/11] Using latest: https://deno.land/x/denops_std@v5.0.1/mod.ts
    [2/11] Looking for releases: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [2/11] Using latest: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [3/11] Looking for releases: https://deno.land/x/unknownutil@v3.4.0/mod.ts
    [3/11] Attempting update: https://deno.land/x/unknownutil@v3.4.0/mod.ts -> v3.6.0
    [3/11] Update successful: https://deno.land/x/unknownutil@v3.4.0/mod.ts -> v3.6.0
    [4/11] Looking for releases: https://deno.land/std@0.195.0/testing/asserts.ts
    [4/11] Attempting update: https://deno.land/std@0.195.0/testing/asserts.ts -> 0.202.0
    [4/11] Update successful: https://deno.land/std@0.195.0/testing/asserts.ts -> 0.202.0
    [5/11] Looking for releases: https://deno.land/x/zod@v3.21.4/mod.ts
    [5/11] Attempting update: https://deno.land/x/zod@v3.21.4/mod.ts -> v3.22.2
    [5/11] Update successful: https://deno.land/x/zod@v3.21.4/mod.ts -> v3.22.2
    [6/11] Looking for releases: https://deno.land/std@0.195.0/streams/text_line_stream.ts
    [6/11] Attempting update: https://deno.land/std@0.195.0/streams/text_line_stream.ts -> 0.202.0
    [6/11] Update successful: https://deno.land/std@0.195.0/streams/text_line_stream.ts -> 0.202.0
    [7/11] Looking for releases: https://deno.land/x/denops_std@v5.0.1/helper/mod.ts
    [7/11] Using latest: https://deno.land/x/denops_std@v5.0.1/helper/mod.ts
    [8/11] Looking for releases: https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts
    [8/11] Using latest: https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts
    [9/11] Looking for releases: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [9/11] Using latest: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [10/11] Looking for releases: https://deno.land/x/denops_std@v5.0.1/mapping/mod.ts
    [10/11] Using latest: https://deno.land/x/denops_std@v5.0.1/mapping/mod.ts
    [11/11] Looking for releases: https://deno.land/x/denops_std@v5.0.1/batch/gather.ts
    [11/11] Using latest: https://deno.land/x/denops_std@v5.0.1/batch/gather.ts
    
    Already latest version:
    https://deno.land/x/denops_std@v5.0.1/mod.ts == v5.0.1
    https://deno.land/x/denops_std@v5.0.1/variable/mod.ts == v5.0.1
    https://deno.land/x/denops_std@v5.0.1/helper/mod.ts == v5.0.1
    https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts == v5.0.1
    https://deno.land/x/denops_std@v5.0.1/variable/mod.ts == v5.0.1
    https://deno.land/x/denops_std@v5.0.1/mapping/mod.ts == v5.0.1
    https://deno.land/x/denops_std@v5.0.1/batch/gather.ts == v5.0.1
    
    Successfully updated:
    https://deno.land/x/unknownutil@v3.4.0/mod.ts v3.4.0 -> v3.6.0
    https://deno.land/std@0.195.0/testing/asserts.ts 0.195.0 -> 0.202.0
    https://deno.land/x/zod@v3.21.4/mod.ts v3.21.4 -> v3.22.2
    https://deno.land/std@0.195.0/streams/text_line_stream.ts 0.195.0 -> 0.202.0

--- denops/openai/deps.ts
+++ denops/openai/deps.ts
@@ -1,9 +1,9 @@
 export * from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
 export * from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
-export * from "https://deno.land/x/unknownutil@v3.4.0/mod.ts";
-export * from "https://deno.land/std@0.195.0/testing/asserts.ts";
-export * from "https://deno.land/x/zod@v3.21.4/mod.ts";
-export { TextLineStream } from "https://deno.land/std@0.195.0/streams/text_line_stream.ts";
+export * from "https://deno.land/x/unknownutil@v3.6.0/mod.ts";
+export * from "https://deno.land/std@0.202.0/testing/asserts.ts";
+export * from "https://deno.land/x/zod@v3.22.2/mod.ts";
+export { TextLineStream } from "https://deno.land/std@0.202.0/streams/text_line_stream.ts";
 export * as helper from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts";
 export * as variable from "https://deno.land/x/denops_std@v5.0.1/variable/mod.ts";
